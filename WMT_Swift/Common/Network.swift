655c549e252a30c27b65826d99f4375bad473dcc
Alok Kumar <iamsinghaloke@gmail.com>
2026-03-22 02:55:02 +0530

    basic app structure is created. following mvvm architecture for better separation of concern handling

---
+++ WMT_Swift/Common/Network.swift
@@ -0,0 +1,16 @@
+//
+//  Network.swift
+//  WMT_Swift
+//
+//  Created by aloksingh on 22/03/26.
+//
+
+import Foundation
+
+enum Network {
+    enum URL: String {
+        case login = "https://reqres.in/api/login"
+        case register = "https://reqres.in/api/register"
+        case logout = "https://reqres.in/api/logout"
+    }
+}
